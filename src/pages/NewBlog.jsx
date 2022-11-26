29e41288a03f6faa5643431b01099688c807be66
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-26 16:40:27 +0100

    new blog add page design added

--- src/pages/NewBlog.jsx
+++ src/pages/NewBlog.jsx
@@ -1,4 +1,64 @@
+import { useState } from "react";
+import { Button, Div, Input, NewBlogContainer } from "../styles/NewBlogStyles";
+
 const NewBlog = () => {
-  return <div>NEWBLOG</div>;
+  //Degiskenler
+  const [title, setTitle] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [explain, setExplain] = useState("");
+
+  //Güncel Tarih
+  const date = new Date().toLocaleDateString("de-DE");
+  console.log(date);
+
+  //fonksiyon
+  const newBlogSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <NewBlogContainer>
+      <form onSubmit={newBlogSubmit}>
+        <Div className="logo">
+          <img
+            src="https://www.transparentpng.com/thumb/technology/png-best-technology-6.png"
+            alt=""
+          />
+          <h3>Add a New Technology</h3>
+        </Div>
+        <Div className="input-container">
+          <Div className="input-groups">
+            <label htmlFor="title">Title</label>
+            <Input
+              type="search"
+              placeholder="Title"
+              id="title"
+              required
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Div>
+          <Div className="input-groups">
+            <label htmlFor="image-url">Image URL</label>
+            <Input
+              type="search"
+              placeholder="Image URL"
+              id="image-url"
+              required
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+          </Div>
+          <Div className="input-groups">
+            <label htmlFor="explain">Explain</label>
+            <textarea
+              id="explain"
+              className="tech-explain"
+              required
+              onChange={(e) => setExplain(e.target.value)}
+            />
+          </Div>
+        </Div>
+        <Button type="submit">Add New</Button>
+      </form>
+    </NewBlogContainer>
+  );
 };
 export default NewBlog;
