58111213eaf46ba2430be6e0667d921f8dcae68d
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-27 23:14:04 +0100

    update and delete method for users added

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,18 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { AuthContext } from "../context/AuthContextProvider";
+
+import {
+  Button,
+  ButtonContainer,
+  DetailsContainer,
+  Div,
+  UpdateButton,
+} from "../styles/DetailsStyles";
 
 const Details = () => {
+  const { currentUser, user } = useContext(AuthContext);
+  console.log("CURRENT USER : ", currentUser);
   //1.YOL USEPARAMSILE IDYI YAKALAYIP SONRA TÜM ELEMANLARIN IDSINI KARSILASTIRIP SADECE ID'SI TUTANIN ÖZELLIKLERINI GÖSTER
   //burdaki id Router'da yazdigimkeyword ile ayni olmak zorunda
   /* const { id } = useParams();
@@ -15,6 +27,38 @@ const Details = () => {
   const item = location.state.element;
   console.log(item);
   const { date, email, id, imageUrl, text, title } = item;
-  return <div>DETAILS</div>;
+  return (
+    <DetailsContainer>
+      <Div>
+        <img src={imageUrl} alt="" />
+        <h1>{title}</h1>
+        <Div
+          style={{
+            backgroundColor: "white",
+            padding: "15px",
+            width: "100%",
+            marginInline: "auto",
+            border: "none",
+          }}
+        >
+          <p>{text}</p>
+          <p>
+            <span style={{ color: "purple" }}>Publish Date :</span> {date}
+          </p>
+          <p>
+            <span style={{ color: "purple" }}>Author :</span> {user.userName}
+          </p>
+        </Div>
+        {user.email === email ? (
+          <ButtonContainer>
+            <Button>Delete</Button>
+            <UpdateButton>Update</UpdateButton>
+          </ButtonContainer>
+        ) : (
+          ""
+        )}
+      </Div>
+    </DetailsContainer>
+  );
 };
 export default Details;
