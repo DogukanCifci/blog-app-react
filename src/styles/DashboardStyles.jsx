876d3f78949e9831256977491a9e0cf30203afd1
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-12-01 15:10:18 +0100

    project completed

--- src/styles/DashboardStyles.jsx
+++ src/styles/DashboardStyles.jsx
@@ -3,10 +3,11 @@ import styled from "styled-components";
 export const DashboardDiv = styled.div`
   //  background-image: url("https://images.pexels.com/photos/327509/pexels-photo-327509.jpeg?auto=compress&cs=tinysrgb&w=4000");
   background-image: url("https://images.pexels.com/photos/450035/pexels-photo-450035.jpeg?auto=compress&cs=tinysrgb&w=4000");
-  padding-top: 70px;
+  padding-top: 110px;
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
+
   h1 {
     text-align: center;
     padding: 20px;
--- src/styles/ProfileStyles.jsx
+++ src/styles/ProfileStyles.jsx
@@ -4,10 +4,15 @@ import styled from "styled-components";
 export const ProfileContainer = styled.div`
   padding-top: 100px;
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   background-color: aliceblue;
+
   .container {
     display: flex;
+    @media (max-width: 1000px) {
+      flex-direction: column;
+    }
     .left-part {
       background-color: #100f0f;
       padding-top: 30px;
@@ -18,12 +23,15 @@ export const ProfileContainer = styled.div`
       align-items: center;
       padding-bottom: 30px;
       min-height: 100vh;
+      @media (max-width: 1000px) {
+        margin-inline: auto;
+      }
       img {
         width: 250px;
         height: 250px;
       }
       h3 {
-        color: purple;
+        color: #f94c66;
         font-size: 30px;
         margin-top: 20px;
         text-decoration: underline;
@@ -31,10 +39,13 @@ export const ProfileContainer = styled.div`
       }
     }
     .right-part {
+      min-width: 500px;
       padding: 30px 50px;
+      margin: 0 100px;
       h1 {
-        margin-bottom: 100px;
+        margin-bottom: 50px;
         text-align: center;
+        color: #839aa8;
       }
     }
   }
@@ -58,14 +69,15 @@ export const Info = styled.div`
   border-bottom: 1px solid gray;
   margin-bottom: 30px;
   span.info {
-    margin-left: 100px;
     font-size: 20px;
+    margin-left: 10px;
     text-align: start;
+    font-weight: 700;
   }
   div.label {
     color: purple;
     font-weight: 700;
     font-size: 24px;
-    margin-bottom: 10px;
+    margin-bottom: 20px;
   }
 `;
